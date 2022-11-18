0783fef6a27498c8cad3ae1ba0707e55bace1133
Ethan Hunt <chessking94@gmail.com>
2022-11-17 22:23:05 -0600

    Process to reclassify time controls

---
+++ ChessWarehouse/dbo.UpdateTimeControlIDsAll.StoredProcedure.sql
@@ -0,0 +1,12 @@
+﻿USE [ChessWarehouse]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [dbo].[UpdateTimeControlIDsAll]
+
+AS
+
+UPDATE dim.TimeControlDetail SET TimeControlID = dbo.GetTimeControlID(TimeControlDetail)
+GO
