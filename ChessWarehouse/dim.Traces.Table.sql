6889588d5a6b3483b40a2a448e406a31ab3aeffc
Ethan Hunt <chessking94@gmail.com>
2022-11-18 16:33:41 -0600

    Add index to lake.Moves.TraceKey and column dim.Traces.Scored

--- ChessWarehouse/dim.Traces.Table.sql
+++ ChessWarehouse/dim.Traces.Table.sql
@@ -7,9 +7,12 @@ GO
 CREATE TABLE [dim].[Traces](
 	[TraceKey] [char](1) COLLATE Latin1_General_CS_AS NOT NULL,
 	[TraceDescription] [varchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	[Scored] [bit] NULL,
  CONSTRAINT [PK_Traces] PRIMARY KEY CLUSTERED 
 (
 	[TraceKey] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
+GO
+ALTER TABLE [dim].[Traces] ADD  DEFAULT ((0)) FOR [Scored]
 GO
