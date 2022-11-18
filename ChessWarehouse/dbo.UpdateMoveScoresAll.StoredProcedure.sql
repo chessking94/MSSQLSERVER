204ff2d880e2c4ea478653fddad0717ceedc1dcc
Ethan Hunt <chessking94@gmail.com>
2022-11-17 22:22:34 -0600

    Process to recalc all move scores

---
+++ ChessWarehouse/dbo.UpdateMoveScoresAll.StoredProcedure.sql
@@ -0,0 +1,28 @@
+﻿USE [ChessWarehouse]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [dbo].[UpdateMoveScoresAll]
+
+AS
+
+UPDATE m
+SET m.Score = CAST(t1.PDF * CAST(POWER(t1.CDF - mp.CDF - 1, 4) AS decimal(10,9)) AS decimal(10,9)),
+	m.MaxScore = t1.PDF
+
+FROM lake.Moves m
+JOIN lake.Games g ON m.GameID = g.GameID
+JOIN dim.TimeControlDetail td ON g.TimeControlDetailID = td.TimeControlDetailID
+LEFT JOIN stat.EvalDistributions t1 ON
+	g.SourceID = t1.SourceID AND
+	td.TimeControlID = t1.TimeControlID AND
+	m.T1_Eval_POV = t1.Evaluation
+LEFT JOIN stat.EvalDistributions mp ON
+	g.SourceID = mp.SourceID AND
+	td.TimeControlID = mp.TimeControlID AND
+	m.Move_Eval_POV = mp.Evaluation
+
+WHERE m.MoveScored = 1
+GO
