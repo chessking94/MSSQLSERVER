6d8ce6a3db6825f1b8575e31ef314a74c3001130
Ethan Hunt <chessking94@gmail.com>
2022-11-18 15:47:45 -0600

    Add Move traces

--- ChessWarehouse/dbo.UpdateMoveScores.StoredProcedure.sql
+++ ChessWarehouse/dbo.UpdateMoveScores.StoredProcedure.sql
@@ -51,4 +51,50 @@ LEFT JOIN FileHistory fh ON
 
 WHERE fh.FileID = @fileid
 AND m.MoveScored = 1
+
+
+;WITH cte AS (
+	SELECT
+	m.GameID,
+	m.MoveNumber,
+	m.ColorID,
+	ROW_NUMBER() OVER (PARTITION BY m.GameID, m.ColorID, LEFT(m.FEN, CHARINDEX(' ', m.FEN) - 1) ORDER BY m.MoveNumber) AS Position_Count
+
+	FROM lake.Moves m
+	JOIN lake.Games g ON
+		m.GameID = g.GameID
+	LEFT JOIN FileHistory fh ON
+		g.FileID = fh.FileID
+
+	WHERE fh.FileID = @fileid
+)
+
+UPDATE m
+SET m.TraceKey = (
+	CASE
+		WHEN m.IsTheory = 1 THEN 'b'
+		WHEN m.IsTablebase = 1 THEN 't'
+		WHEN m.T1_Eval_POV IS NULL OR ABS(m.T1_Eval_POV) > CAST(s.Value AS decimal(5,2)) THEN 'e'
+		WHEN cte.Position_Count > 1 THEN 'r'
+		WHEN m.T2_Eval IS NULL OR ABS(CAST(m.T1_Eval AS decimal(5,2)) - (CASE WHEN LEFT(m.T2_Eval, 1) = '#' THEN 100 ELSE CAST(m.T2_Eval AS decimal(5,2)) END)) > 2.00 THEN 'f'
+		WHEN m.Move_Rank = 1 THEN 'M'
+		ELSE '0'
+	END
+)
+
+FROM lake.Moves m
+JOIN lake.Games g ON
+	m.GameID = g.GameID
+LEFT JOIN FileHistory fh ON
+	g.FileID = fh.FileID
+JOIN dim.Colors c ON
+    m.ColorID = c.ColorID
+CROSS JOIN Settings s
+JOIN cte ON
+	m.GameID = cte.GameID AND
+	m.MoveNumber = cte.MoveNumber AND
+	m.ColorID = cte.ColorID
+
+WHERE s.ID = 3
+AND fh.FileID = @fileid
 GO
