1cd9b08b32870bb5c0d01421ba7889c6308e353f
Ethan Hunt <chessking94@gmail.com>
2022-11-27 13:42:34 -0600

    Add fields to log previous values

--- ChessWarehouse/stat.EvalDistributionParameters.Table.sql
+++ ChessWarehouse/stat.EvalDistributionParameters.Table.sql
@@ -7,14 +7,19 @@ GO
 CREATE TABLE [stat].[EvalDistributionParameters](
 	[SourceID] [tinyint] NOT NULL,
 	[TimeControlID] [tinyint] NOT NULL,
+	[UpdateDate] [datetime] NOT NULL,
 	[Mean] [decimal](18, 15) NOT NULL,
 	[StandardDeviation] [decimal](18, 15) NOT NULL,
+	[PrevMean] [decimal](18, 15) NULL,
+	[PrevStandardDeviation] [decimal](18, 15) NULL,
  CONSTRAINT [PK_EvalDistributionParameters] PRIMARY KEY CLUSTERED 
 (
 	[SourceID] ASC,
 	[TimeControlID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
+GO
+ALTER TABLE [stat].[EvalDistributionParameters] ADD  CONSTRAINT [DF_EvalDistributionParameters_UpdateDate]  DEFAULT (getdate()) FOR [UpdateDate]
 GO
 ALTER TABLE [stat].[EvalDistributionParameters]  WITH CHECK ADD  CONSTRAINT [FK_EvalDistributionParameters_SourceID] FOREIGN KEY([SourceID])
 REFERENCES [dim].[Sources] ([SourceID])
