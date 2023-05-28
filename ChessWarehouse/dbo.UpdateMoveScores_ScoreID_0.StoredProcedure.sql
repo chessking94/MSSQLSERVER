f3ff3c3f221f4683949089b147b0c44b1fe483f4
Ethan Hunt <chessking94@gmail.com>
2023-05-28 13:42:03 -0500

    Dummy proc for test score record

---
+++ ChessWarehouse/dbo.UpdateMoveScores_ScoreID_0.StoredProcedure.sql
@@ -0,0 +1,26 @@
+﻿USE [ChessWarehouse]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [dbo].[UpdateMoveScores_ScoreID_0] (@FileID int = NULL)
+
+AS
+
+UPDATE ms
+SET ms.ScoreValue = NULL,
+	ms.MaxScoreValue = NULL
+
+FROM stat.MoveScores ms
+JOIN lake.Moves m ON
+	ms.GameID = m.GameID AND
+	ms.MoveNumber = m.MoveNumber AND
+	ms.ColorID = m.ColorID
+JOIN lake.Games g ON m.GameID = g.GameID
+LEFT JOIN FileHistory fh ON
+	g.FileID = fh.FileID
+
+WHERE m.MoveScored = 1
+AND (fh.FileID = @FileID OR ISNULL(@FileID, -1) = -1)
+GO
